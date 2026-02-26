351d3c3d90537ea08ba567be78f66bbaf6e1eeba
DhavalTank01 <dhavaltank2001@gmail.com>
2026-02-26 22:16:03 +0530

    initial commit
    initial commit

---
+++ src/constants/strings.ts
@@ -0,0 +1,22 @@
+class Strings {
+  static github = "GitHub";
+  static githubLink = "https://www.github.com/dhavaltank";
+  static githubUsername = "dhavaltank02";
+
+  static linkedIn = "LinkedIn";
+  static linkedInLink = "https://www.linkedin.com/in/dhaval-tank-0950ba177/";
+  static linkedInUsername = "dhavaltank";
+
+  static instagram = "Instagram";
+  static instagramLink = "https://www.instagram.com/dhaval.tank.01";
+  static instagramUsername = "dhavaltank";
+
+  static email = "Email";
+  static primaryEmailLink = "mailto:dhavaltank2001@gmail.com";
+  static primaryEmail = "dhavaltank2001@gmail.com";
+
+  static fullName = "Dhaval Tank";
+  static shortName = "dhavaltank";
+}
+
+export default Strings;
